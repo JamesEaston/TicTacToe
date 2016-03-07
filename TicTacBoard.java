801e94a4a4c5deedf7fd4d3e73150826b965598a
JREAST01 <jreast01@jreast01-VirtualBox.(none)>
2016-03-07 14:08:33 -0500

    Created the checkWin method

--- TicTacBoard.java
+++ TicTacBoard.java
@@ -44,7 +44,35 @@ class TicTacBoard{
 		return output;
 	}
 
-	public int checkWin(){
-		
+	public boolean checkWin(int player){
+		int rowCheck = 0;
+		int colCheck = 0;
+		int diagCheckOne = 0;
+		int diagCheckTwo = 0;
+		for (int i = 0; i < board.length; i++){
+			for (int v = 0; v < board.length; v++){
+				if (board[i][v] == player){
+					rowCheck++;
+				}
+				if (board[v][i] == player){
+					colCheck++;
+				}
+			}
+			if (rowCheck == 3 || colCheck == 3){
+				return true;
+			}
+			rowCheck = 0;
+			colCheck = 0;
+			if (board[i][i] == player){
+				diagCheckOne++;
+			}
+			if (board[board.length - 1][i] == player){
+				diagCheckTwo++;
+			}
+		}
+		if (diagCheckOne == 3 || diagCheckTwo == 3){
+			return true;
+		}
+		return false;
 	}
 }
