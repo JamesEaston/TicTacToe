8ebf7a786204eac97e54a05fa743ce0cf2730ec0
JREAST01 <jreast01@jreast01-VirtualBox.(none)>
2016-03-07 10:48:17 -0500

    Initial commit

---
+++ Client.java
@@ -0,0 +1,8 @@
+public class Client{
+	public static void main(String[] args){
+		TicTacBoard board = new TicTacBoard();
+		System.out.println(board.toString());
+		System.out.println(" | ");
+		System.out.println("-+-");
+	}
+}
---
+++ TicTacBoard.java
@@ -0,0 +1,46 @@
+class TicTacBoard{
+
+	private int[][] board;
+
+	public TicTacBoard(){
+		board = new int[5][5];
+		for(int row = 0; row < board.length; row++){
+			for(int col = 0; col < board.length; col++){
+				if(!(row % 2 == 0)){
+					board[row][col] = 3;
+				} 
+				else if(!(col % 2 == 0)){
+					board[row][col] = 4;
+				}
+				else{
+					board[row][col] = 0;
+				}
+			}
+		}
+	}
+
+	public String toString() {
+		String output = "";
+		for(int row = 0; row < board.length; row++){
+			for(int col = 0; col < board[row].length; col++){
+				if(board[row][col] == 0){
+					output += " ";
+				}
+				if(board[row][col] == 1){
+					output += "X";
+				}
+				if(board[row][col] == 2){
+					output += "O";
+				}
+				if(board[row][col] == 3){
+					output += "|";
+				}
+				if(board[row][col] == 4){
+					output += "-";
+				}
+			}
+			output += "\n";
+		}
+		return output;
+	}
+}
